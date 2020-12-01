fb7e90c13349384e36329cf62a9ed5553744eb83
plurmp <plurmpmcflurnt@gmail.com>
2020-12-01 12:09:58 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+from os import environ as cred
+
+import discord
+
+client = discord.Client
+TOKEN = cred['DISCORD_TOKEN']
+
+
+@client.event
+async def on_message(message):
+	for r in message.role_mentions:
+		if r.int == 783392898619408425:  # @someone
+			members = message.guild
+			victim = members[random.randrange(len(members) - 1)]
+			message.channel.send(victim.mention())
+			return
+
+client.run(TOKEN)
